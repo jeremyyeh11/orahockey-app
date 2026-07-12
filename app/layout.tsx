733d3ce183003d06a7c1750f391be14634ebbb0d
jem <133052471+jeremyyeh11@users.noreply.github.com>
2026-07-12 20:18:06 +0800

    Disable iOS zoom: maximumScale=1, userScalable=false

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,8 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
   themeColor: '#0f172a',
 }
 
